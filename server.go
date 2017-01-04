4500d46ff7e30164795fab8cfde158104922ce5d
nc <charles.saternos@gmail.com>
2017-01-04 14:15:39 -0500

    added file tree and viewing blobs

--- server.go
+++ server.go
@@ -32,18 +32,19 @@ func renderPage(repo *git.Repository, w http.ResponseWriter, t *template.Templat
 	})
 }
 
+// TODO: get commit log for an arbitrary branch
 func getCommitLog(repo *git.Repository) []*git.Commit {
 	r, err := repo.Walk()
 	if err != nil {
 		log.Print("failed to walk repo: ", err)
 	}
 
-	headObj, err := repo.RevparseSingle("HEAD")
+	masterObj, err := repo.RevparseSingle("master")
 	if err != nil {
-		log.Print("failed to get HEAD: ", err)
+		log.Print("failed to get master: ", err)
 	}
 
-	r.Push(headObj.Id())
+	r.Push(masterObj.Id())
 	r.Sorting(git.SortTime)
 	r.SimplifyFirstParent()
 
@@ -79,6 +80,52 @@ func getCommitPatches(repo *git.Repository, commit *git.Commit) []string {
 	return r
 }
 
+func readFile(repo *git.Repository, filename string) (string, error) {
+	masterObj, err := repo.RevparseSingle("master")
+	if err != nil {
+		log.Print("failed to get master: ", err)
+	}
+
+	c, err := masterObj.AsCommit()
+	if err != nil {
+		log.Print("invalid commit: ", err)
+	}
+
+	t, err := c.Tree()
+	if err != nil {
+		log.Print("invalid tree: ", err)
+	}
+
+	te := t.EntryByName(filename)
+	if te == nil {
+		log.Print("no file: ", filename)
+		return "", fmt.Errorf("no such file/blob/tree entry %g", filename)
+	}
+
+	f, err := repo.Lookup(te.Id)
+	if err != nil {
+		log.Print("invalid file: ", err)
+	}
+
+	b, err := f.AsBlob()
+	if err != nil {
+		log.Print("invalid blob: ", err)
+	}
+
+	return string(b.Contents()), nil
+}
+
+func getFileTree(repo *git.Repository) []string {
+	masterObj, _ := repo.RevparseSingle("master")
+	c, _ := masterObj.AsCommit()
+	t, _ := c.Tree()
+	var r []string
+	for i := uint64(0); i < t.EntryCount(); i++ {
+		r = append(r, t.EntryByIndex(i).Name)
+	}
+	return r
+}
+
 func main() {
 	repoPath := "../dirt/"
 	repo, err := git.OpenRepository(repoPath)
@@ -88,13 +135,22 @@ func main() {
 	}
 
 	templates := template.Must(template.ParseGlob("t/*"))
+
 	if err != nil {
 		log.Print("couldn't parse templates: ", err)
 	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderPage(repo, w, templates, template.HTML(repoPath))
+		ft := getFileTree(repo)
+
+		html_buffer := bytes.NewBufferString("")
+		fmt.Fprintf(html_buffer, "<table>")
+		for _, v := range ft {
+			fmt.Fprintf(html_buffer, "<tr><td><a href='/blob/%s'>%s</a></tr>", v, v)
+		}
+		fmt.Fprintf(html_buffer, "</table>")
+		renderPage(repo, w, templates, template.HTML(html_buffer.String()))
 	})
 
 	r.HandleFunc("/log/", func(w http.ResponseWriter, r *http.Request) {
@@ -119,6 +175,16 @@ func main() {
 		d := getCommitPatches(repo, c)
 
 		renderPage(repo, w, templates, template.HTML("<h1>Diff</h1><pre>"+strings.Join(d, "\n")+"</pre>"))
+	})
+
+	r.HandleFunc("/blob/{filename}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		s, err := readFile(repo, vars["filename"])
+		if err != nil {
+			s = err.Error()
+		}
+
+		renderPage(repo, w, templates, template.HTML("<h1>"+vars["filename"]+"</h1><pre>"+s+"</pre>"))
 	})
 
 	http.Handle("/", r)
